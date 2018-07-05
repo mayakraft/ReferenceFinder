86ba66b92f81c0d151645218da57eefea75f7566
Robby Kraft <robbykraft@gmail.com>
2018-07-05 18:23:47 -0400

    cross origin

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,16 +2,20 @@
 module.exports = function(app) {
 	var refFinder = require('../controllers/controller');
 	
-	app.all('/', function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "X-Requested-With");
-		next();
-	});
+	// app.all('/', function(req, res, next) {
+	// 	res.header("Access-Control-Allow-Origin", "*");
+	// 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	// 	next();
+	// });
 
 	app.route('/').get(refFinder.listServices);
 
 	app.route('/point')
 		.get(refFinder.solvePoint)
 		.post(refFinder.postPoint);
+
+	// app.listen(80, function () {
+	// 	console.log('CORS-enabled web server listening on port 80')
+	// })
 
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
 var express = require('express'),
+	cors = require('cors'),
 	app = express(),
 	port = process.env.PORT || 3000,
 	bodyParser = require('body-parser');
 
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
